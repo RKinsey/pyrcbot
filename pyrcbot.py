6091781b4eb477af6713befa05108659dd24e585
ross <Ross K>
2014-11-05 22:09:30 -0500

    added some modularity of names and the like, also added some commands

--- pyrcbot.py
+++ pyrcbot.py
@@ -1,24 +1,33 @@
 import socket
-
+#command sequence is currently mb
+server="irc.chatspike.net"
 channel="#DesertBusBunker"
-nick = "MoatBort"
+nick = "MoatBot"
+nspass=raw_input("please enter your NS password")
+#server=raw_input("What server?")
+#channel=raw_input("Please enter the channel to join")
+#nick = raw_input("Bot Nickname?")
+#nspass=raw_input("Please enter the nickServ password")
 
 conn=socket.socket()
-conn.connect(("178.62.119.156",6667))
+conn.connect((server,6667))
 conn.send("PASS secret\n")
 conn.send("NICK "+nick+"\n")
 conn.send("USER moatbot moatbot moatbot :learning\n")
-
-count=0
-while count<10:
+flag=0
+while True:
     recieved=conn.recv(1024)
     print recieved
-    if recieved.find("PING :")!=-1:
-        conn.send("PONG :pingis\n")
-    count+=1
-conn.send("JOIN "+channel+"\n")
-conn.send("PRIVMSG "+channel+" :"+"Whoo\n")
+    if flag==0 and recieved.find("MODE")!=-1:
+        conn.send("PRIVMSG NickServ :IDENTIFY "+nspass+"\n")
+        conn.send("JOIN "+channel+"\n")
+        flag=1
+    if recieved.find(":mb test")!=-1:
+        conn.send("PRIVMSG "+channel+ " :Hello\n")
+    if recieved.find(":mb quit")!=-1:
+        conn.send("QUIT :leaving")
+        break
+    
 
-conn.send("QUIT :done\n")
 conn.close()
 
