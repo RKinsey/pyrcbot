ced4e33508f8fe1144fbcb99db3d669badfff3b4
ross <Ross K>
2014-11-05 20:46:04 -0500

    fixed some formatting

--- pyrcbot.py
+++ pyrcbot.py
@@ -1,7 +1,8 @@
 import socket
-#server="irc.chatspike.net"
+
 channel="#DesertBusBunker"
 nick = "MoatBot"
+
 conn=socket.socket()
 conn.connect(("178.62.119.156",6667))
 conn.send("PASS 14620\n")
@@ -9,7 +10,7 @@ conn.send("NICK "+nick+"\n")
 conn.send("USER moatbot moatbot moatbot :learning\n")
 conn.send("JOIN "+channel+"\n")
 conn.send("PRIVMSG "+channel+" :"+"Whoo\n")
- 
+
 while 1:
     recieved=conn.recv(1024)
     print recieved
