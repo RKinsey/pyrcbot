4037e9f0d6cff7fad30d935eeaf108c2737d576c
ross <Ross K>
2014-11-05 21:39:32 -0500

    Fixed a connection bug

--- pyrcbot.py
+++ pyrcbot.py
@@ -1,18 +1,24 @@
 import socket
 
 channel="#DesertBusBunker"
-nick = "MoatBot"
+nick = "MoatBort"
 
 conn=socket.socket()
 conn.connect(("178.62.119.156",6667))
-conn.send("PASS 14620\n")
+conn.send("PASS secret\n")
 conn.send("NICK "+nick+"\n")
 conn.send("USER moatbot moatbot moatbot :learning\n")
+
+count=0
+while count<10:
+    recieved=conn.recv(1024)
+    print recieved
+    if recieved.find("PING :")!=-1:
+        conn.send("PONG :pingis\n")
+    count+=1
 conn.send("JOIN "+channel+"\n")
 conn.send("PRIVMSG "+channel+" :"+"Whoo\n")
 
-while 1:
-    recieved=conn.recv(1024)
-    print recieved
+conn.send("QUIT :done\n")
 conn.close()
 
